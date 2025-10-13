8b6c7a285363627fd61a8054e60f2f3a5fd4bde5
mrsalag1967 <mrsalag@hotmail.com>
2025-10-13 15:48:44 -0400

    Primera versión del sitio SkyAILogic

---
+++ script.js
@@ -0,0 +1,15 @@
+// Puedes usar este archivo más adelante para animaciones o interactividad
+console.log("Portafolio cargado correctamente ✅");
+Para la otra opcion de la venta de codigo fuente en que lenguajes lo entrega en PHP o dot.NET
+
+
+ustedes lo puede ser desarrollado en PHP o Dot.NET 
+y cuales son las Bases de Datos que ustedes utilizan
+
+LinkedIn
+Innova Code, Neura Soft. Futuro Code, InnovAI, Conecta AI, MenteDigital, CodigoEficaz, MenteTec, Brillantech. SmartCode
+Innova Code, Neura Soft. Futuro Code, InnovAI, Conecta AI, MenteDigital, CódigoEficaz, MenteTec, Brillantech. CódigoInteligente
+
+DigitalMind
+SmartCode
+SkyAILogic
